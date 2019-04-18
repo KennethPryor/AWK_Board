dbfb86be663ef4e26aee682abf7237d33bec6195
KennethPryor <kennypryor131@gmail.com>
2019-04-18 16:03:36 -0400

    got daily board up and running

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ var cars_sold = "";
 
 var salespersonRef = database.ref('Salespersons');
 var trackingBoardRef = database.ref('tracking_board');
-console.log(trackingBoardRef.new);
+var dailyBoardRef = database.ref('daily_board');
 
 salespersonRef.once('value')
   .then(function (snapshot) {
@@ -42,15 +42,47 @@ $("#add-user").on("click", function (event) {
 
   // Grabbed values from text-boxes
   name = $("#name-input").val().trim();
-  cars_sold = $("#cars_sold-input").val().trim();
 
   // Code for "Setting values in the database"
   salespersonRef.push({
     name: name,
-    cars_sold: cars_sold,
+    cars_sold: 0,
   });
 
 });
+
+$("#add-daily_board").on("click", function (event) {
+  event.preventDefault();
+
+  // Grabbed values from text-boxes
+  salesperson = $("#salesperson_name-input").val().trim();
+  customer = $("#customer_name-input").val().trim();
+  stock_num = $("#stock_num-input").val().trim();
+  trade = $("#trade_in-input").val().trim();
+
+  // Code for "Setting values in the database"
+  dailyBoardRef.push({
+    salesperson: salesperson,
+    customer: customer,
+    stock_num: stock_num,
+    trade: trade,
+  });
+});
+
+dailyBoardRef.once('value')
+  .then(function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      var childData = childSnapshot.val();
+      $('#daily_board-tbody').append(`
+        <tr>
+            <td>${childData.salesperson}</td>
+            <td>${childData.customer}</td>
+            <td>${childData.stock_num}</td>
+            <td>${childData.trade}</td>
+        </tr>
+    `);
+    });
+  });
 
 // Firebase watcher + initial loader HINT: .on("value")
 salespersonRef.on("value", function (snapshot) {
