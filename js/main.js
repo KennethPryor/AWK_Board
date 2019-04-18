a3c6de1d85f3f14364196013be443a085938ce08
KennethPryor <kennypryor131@gmail.com>
2019-04-18 15:27:38 -0400

    got board up and running, need to get admin page working now

---
+++ js/main.js
@@ -0,0 +1,100 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD53WlXc8pGKKZKLtmzlYYqvGgp0FWJA5Q",
+  authDomain: "awk-board.firebaseapp.com",
+  databaseURL: "https://awk-board.firebaseio.com",
+  projectId: "awk-board",
+  storageBucket: "awk-board.appspot.com",
+  messagingSenderId: "736569817172"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// Initial Values
+var name = "";
+var cars_sold = "";
+
+var salespersonRef = database.ref('Salespersons');
+var trackingBoardRef = database.ref('tracking_board');
+console.log(trackingBoardRef.new);
+
+salespersonRef.once('value')
+  .then(function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      var key = childSnapshot.key;
+      var childData = childSnapshot.val();
+      console.log('CD' + childData)
+      console.log('Key: ' + key);
+      console.log('Data:' + childData.name);
+      $('#salesperson_tbody').append(`
+        <tr>
+            <td>${childData.name}</td>
+            <td>${childData.cars_sold}</td>
+        </tr>
+    `);
+    });
+  });
+
+// Capture Button Click
+$("#add-user").on("click", function (event) {
+  event.preventDefault();
+
+  // Grabbed values from text-boxes
+  name = $("#name-input").val().trim();
+  cars_sold = $("#cars_sold-input").val().trim();
+
+  // Code for "Setting values in the database"
+  salespersonRef.push({
+    name: name,
+    cars_sold: cars_sold,
+  });
+
+});
+
+// Firebase watcher + initial loader HINT: .on("value")
+salespersonRef.on("value", function (snapshot) {
+
+  // Log everything that's coming out of snapshot
+  console.log(snapshot.val());
+  console.log(snapshot.val().name);
+  console.log(snapshot.val().cars_sold);
+  // Change the HTML to reflect
+  $("#name-display").text(snapshot.val().name);
+  $("#cars_sold-display").text(snapshot.val().cars_sold);
+
+  // Handle the errors
+}, function (errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
+
+trackingBoardRef.on("value", function (snapshot) {
+
+  $("#new-display").text(snapshot.val().new);
+  $("#tracking_new-display").text(snapshot.val().tracking_new);
+  $("#used-display").text(snapshot.val().used);
+  $("#tracking_used-display").text(snapshot.val().tracking_used);
+  $("#total_sold-display").text(snapshot.val().total_sold);
+  $("#tracking_total-display").text(snapshot.val().tracking_total);
+
+  // Handle the errors
+}, function (errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
+
+
+// salespersonArray.forEach(function (i) {
+
+//     $('#salesperson_tbody').append(`
+//         <tr>
+//             <td>${$("#name-display").text(snapshot.val().name)}</td>
+//             <td>${$("#cars_sold-display").text(snapshot.val().cars_sold)}</td>
+//         </tr>
+//     `);
+// });
+
+// day of the month/days in the month
+
+// /amount of cars sold
+
+// =tracking number
